75048f03fe3be2f1c4276381896f4e9bee8eb683
fuxiaojie <457732323@qq.com>
2018-11-12 21:37:32 +0800

    最长回文子串长度

---
+++ algorithm/com.string/LongestPalindrome.java
@@ -0,0 +1,45 @@
+package com.string;
+
+/**
+ * Created by lenovo on 2018/11/12.
+ */
+public class LongestPalindrome {
+    public static int longestPalindrome(String s) {
+        int length = s.length();
+        // max：最长回文串的长度
+        // c：当前回文串的长度
+        int i, j, max, c = 0;
+        if(length == 0 || s == null)
+            return 0;
+        max = 0;
+        // 回文串长度为奇数
+        for(i = 0; i < length; i++) {
+            for(j = 0; (i - j) >= 0 && (i + j) < length; j++) {
+                if(s.charAt(i - j) != s.charAt(i + j))
+                    break;
+                c = j * 2 + 1;
+            }
+            if(c > max)
+                max = c;
+        }
+
+        // 回文串长度为偶数
+        for(i = 0; i < length; i++) {
+            for(j = 0; (i - j) >= 0 && (i + j + 1) < length; j++) {
+                if(s.charAt(i - j) != s.charAt(i + j + 1))
+                    break;
+                c = j * 2 + 2;
+            }
+            if(c > max)
+                max = c;
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "abcdcba";
+        System.out.println(LongestPalindrome.longestPalindrome(s));
+    }
+}
