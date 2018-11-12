e549d5f731da88e86b3040c7037923ed62c13daa
fuxiaojie <457732323@qq.com>
2018-11-12 15:58:04 +0800

    判断一条单向链表是不是回文

--- algorithm/com.string/Palindrome_2.java
+++ algorithm/com.string/Palindrome_2.java
@@ -11,21 +11,82 @@ package com.string;
  */
 public class Palindrome_2 {
 
-    class Node {
-        Node next = null;// 节点的引用，指向下一个节点
-        int data;// 节点的对象，即内容
 
-        public Node(int data) {
-            this.data = data;
-        }
-    }
 
     public static boolean isPalidrome(Node node) {
+        Node temp = node;
+        Node first = node;
+        int count = 0;
+        while(temp.next != null) {
+            count ++;
+            temp = temp.next;
+        }
+        count = count / 2 ;
+        temp = node;
+        while(count -- != 0) {
+            temp = temp.next;
+        }
+        temp = reverse(temp);
 
+
+        if(temp.next != null)
+            temp = temp.next;
+        while(temp != null) {
+
+            if(first.data != temp.data)
+                return false;
+            temp = temp.next;
+            first = first.next;
+        }
         return true;
     }
 
-    public static void main(String[] args) {
+    private static Node reverse(Node node) {
+        Node head = node;
+        Node p = new Node();
+        Node q = new Node();
+        if(head.next != null)
+            p = head.next;
+        while(p.next != null) {
+            q = p.next;
+            p.next = q.next;
+            // 这个地方容易出错
+            q.next = head.next;
+            head.next = q;
+        }
 
+        return head;
+    }
+    public static void main(String[] args) {
+        Palindrome_2 palindrome2 = new Palindrome_2();
+        Node node1 = new Node(0);
+        Node node2 = new Node(1);
+        Node node3 = new Node(1);
+        Node node4 = new Node(0);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = null;
+
+
+        /*Node resu = palindrome2.reverse(node3);
+        while(resu != null){
+            System.out.print(resu.data + ", ");
+            resu = resu.next;
+        }*/
+        System.out.println(isPalidrome(node1));
+    }
+}
+
+class Node {
+    Node next = null;// 节点的引用，指向下一个节点
+    int data;// 节点的对象，即内容
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node() {
     }
 }
